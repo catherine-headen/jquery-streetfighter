31e132a3606a6e2cceea4454714344142ed58375
Catherine Headen <catherine.headen@gmail.com>
2015-11-04 14:41:04 -0500

    Making Ryu Move

--- js/app.js
+++ js/app.js
@@ -4,7 +4,18 @@ $(document).ready(function() {
     $('.ryu-ready').show();
   })
   .mouseleave(function() {
-	$('.ryu-ready').hide();
-	$('.ryu-still').show();
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    // play hadouken sound
+  	$('.ryu-ready').hide();
+  	$('.ryu-throwing').show();
+  	$('.hadouken').show();
+  	// animate hadouken to the right of the screen
+  })
+  .mouseup(function() {
+  	$('.ryu-throwing').hide();
+  	$('.ryu-ready').show();
   });
 });
